8d9af400bf305456c5603ba15ab61c1e27a06929
Yoon Doohyun <ydh2500@gmail.com>
2024-04-09 11:31:19 +0900

    추가 조치

--- naver_api.py
+++ naver_api.py
@@ -4,7 +4,6 @@ from auth.token_manager import get_token
 from domain.order.order_manager import NaverOrderManager
 
 access_token = get_token()
-
 order_manager = NaverOrderManager()
 
 def get_connection():
